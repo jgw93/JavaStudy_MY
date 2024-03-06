d1baaa8afd829718148d069f1facdde2f3e8f648
jgw1993 <kmmh99@naver.com>
2024-03-06 10:17:46 +0900

    완료시 보여질 메시지

---
+++ JavaProject240228/src/MY_240228/Test_1.java
@@ -0,0 +1,16 @@
+package MY_240228;
+
+public class Test_1 {
+	// 파일 명과 클래스명은 동일해야하고,
+	// 만약, 틀린다면, 문법 검사기(컴파일러)가
+	// 요류를 체크 해줍니다.
+	// 모든 문법을 다 알려고 안해도 된다.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// syso 다음에 ctrl + space, 자동완성.
+		// 실행, ctrl + F11
+        System.out.println("Hi");
+	}
+
+}
---
+++ JavaProject240228/src/ex_240304/Ex_03_Class_2_Idol.java
@@ -0,0 +1,37 @@
+package ex_240304;
+
+import java.util.Scanner;
+
+public class Ex_03_Class_2_Idol {
+
+	public static void main(String[] args) {
+		// Idol 클래스 사용
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("아이돌 그룹명을 입력해주세요.>>");
+		String groupNameString = scanner.next();
+		
+		System.out.println("그룹은 몇명입니까?>>");
+		int groupMemberNumber = scanner.nextInt();
+
+		System.out.println("아이돌 그룹멤버를 입력해주세요.(예, 공백으로 구분해서 넣기)>>");
+		String[] groupMember = new String[groupMemberNumber];
+		for(int i=0; i<groupMember.length; i++) {
+			groupMember[i] = scanner.next();
+		}
+		
+		System.out.println("대표 앨범 3개만 입력해주세요.(예, 공백으로 구분해서 넣기)>>");
+		String[] groupAlbum = new String[3];
+		for(int i=0; i<groupAlbum.length; i++) {
+			groupAlbum[i] = scanner.next();
+		}
+		
+		// Idol(String groupName, String[] groupMember, String[] groupAlbums)
+		// BTS
+		Idol Idol = new Idol(groupNameString, groupMember, groupAlbum);
+		Idol.introduceMember();
+		Idol.introduceAlbum();
+		
+
+	}
+
+}
